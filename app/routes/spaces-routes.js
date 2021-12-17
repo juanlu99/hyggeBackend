669b49cf68f224acc1d20418d7840743de7bbd6b
Pablo Pazos <ppazosseoane@gmail.com>
2021-12-17 13:44:43 +0100

    Añadido en su mayoria crear una review

--- app/routes/spaces-routes.js
+++ app/routes/spaces-routes.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const express = require(`express`);
+const createreviewSpaceById = require("../controllers/spaces/create-review-by-space-id-controller");
 const getSpaces = require("../controllers/spaces/getSpacesController");
 const router = express.Router();
 
 // Endpoints públicos
 router.route(`/`).get(getSpaces);
+router.route(`/:spaceId/reviews`).post(createreviewSpaceById);
 
 // Endpoints privados
 
