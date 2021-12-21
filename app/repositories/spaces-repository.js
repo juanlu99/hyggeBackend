4d1ee81b9ad6c385d2f741e657abadefb05458a0
juanlu99 <j.l.mendez.alvarez@gmail.com>
2021-12-21 20:09:28 +0100

    Changed sql and repositories to english

--- app/repositories/spaces-repository.js
+++ app/repositories/spaces-repository.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
 const getPool = require(`../infrastructure/database`);
 
 async function findAllSpaces() {
   const pool = await getPool();
-  const sql = `SELECT * FROM espacios`;
+  const sql = `SELECT * FROM spaces`;
   const [spaces] = await pool.query(sql);
   return spaces;
 }
 
 async function findSpaceById(id) {
   const pool = await getPool();
-  const sql = "SELECT * FROM espacios WHERE idEspacio = ?";
+  const sql = 'SELECT * FROM spaces WHERE idSpace = ?';
   const [spaces] = await pool.query(sql, id);
   return spaces[0];
 }
--- app/repositories/users-repository.js
+++ app/repositories/users-repository.js
@@ -2,11 +2,10 @@
 
 const getPool = require('../infrastructure/database');
 
-
 async function createUser(user) {
   const pool = await getPool();
   const sql = `
-  insert into users (nombre, email, password, verificationCode, role, createdAt)
+  insert into users (name, email, password, verificationCode, role, createdAt)
   values (?, ?, ?, ?, ?, ?)
   `;
   const { name, email, passwordHash, verificationCode } = user;
@@ -15,11 +14,10 @@ async function createUser(user) {
   return created.insertId;
 }
 
-
 async function findUserByEmail(email) {
   const pool = await getPool();
   const sql = `
-  select idUser, nombre, email, password, role, verifiedAt from users where email = ?
+  select idUser, name, email, password, role, verifiedAt from users where email = ?
   `;
   const [user] = await pool.query(sql, email);
   return user[0];
