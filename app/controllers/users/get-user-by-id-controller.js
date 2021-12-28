a5a03ed58a8af2588bd1073621e22b6d2d68c4d9
juanlu99 <89475173+juanlu99@users.noreply.github.com>
2021-12-28 19:21:06 +0100

    Update get-user-by-id-controller.js

--- app/controllers/users/get-user-by-id-controller.js
+++ app/controllers/users/get-user-by-id-controller.js
@@ -5,7 +5,7 @@ const createJsonError = require('../../errors/create-json-error');
 const throwJsonError = require('../../errors/throw-json-error');
 const { findUserByID } = require('../../repositories/users-repository');
 const isAdmin = require('../../helpers/isAdmin');
-const schema = Joi.number().positive().required();
+const schema = Joi.number().positive().integer().required();
 
 async function getUserByID(req, res) {
   try {
