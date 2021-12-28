8c71c3dd1317bd7da6e15e7549c67408af38904c
juanlu99 <j.l.mendez.alvarez@gmail.com>
2021-12-28 17:34:40 +0100

    getProfileByID (admin only) added

---
+++ app/controllers/users/get-user-by-id-controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Joi = require('joi');
+const createJsonError = require('../../errors/create-json-error');
+const throwJsonError = require('../../errors/throw-json-error');
+const { findUserByID } = require('../../repositories/users-repository');
+const isAdmin = require('../../helpers/isAdmin');
+const schema = Joi.number().positive().required();
+
+async function getUserByID(req, res) {
+  try {
+    await isAdmin(req);
+    const { id } = req.params;
+    await schema.validateAsync(id);
+    const user = await findUserByID(id);
+    if (!user) {
+      throwJsonError(401, 'Este usuario no existe.');
+    }
+    res.status(200).send({ user });
+  } catch (error) {
+    createJsonError(error, res);
+  }
+}
+
+module.exports = getUserByID;
