0fb48e1b9b77e6dcd9ef0a7d3e01064cdb65c99b
Pablo Pazos <ppazosseoane@gmail.com>
2022-01-17 11:58:19 +0100

    añadido el endopint privado

--- app/routes/spaces-routes.js
+++ app/routes/spaces-routes.js
@@ -3,12 +3,13 @@
 const express = require(`express`);
 const createreviewSpaceById = require("../controllers/spaces/create-review-by-space-id-controller");
 const getSpaces = require("../controllers/spaces/getSpacesController");
+const validateAuth = require("../middlewares/validate-auth");
 const router = express.Router();
 
 // Endpoints públicos
 router.route(`/`).get(getSpaces);
 
 // Endpoints privados
-router.route(`/:spaceId/reviews`).post(createreviewSpaceById);
+router.route(`/:spaceId/reviews`).all(validateAuth).post(createreviewSpaceById);
 
 module.exports = router;
