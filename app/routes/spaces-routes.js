dae1dfde9adfb3a014ad100a3e468028694d5d46
juanlu99 <j.l.mendez.alvarez@gmail.com>
2022-01-17 12:44:51 +0100

    lets goo

--- app/routes/spaces-routes.js
+++ app/routes/spaces-routes.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const express = require(`express`);
-const createreviewSpaceById = require("../controllers/spaces/create-review-by-space-id-controller");
-const {
-  getReviewsBySpaceId,
-} = require("../controllers/spaces/get-reviews-by-space-id");
+const createReviewSpaceById = require('../controllers/spaces/create-review-by-space-id-controller');
+const { getReviewsBySpaceId } = require('../controllers/spaces/get-reviews-by-space-id');
 const deleteSpaceByID = require('../controllers/spaces/delete-space-by-id-controller');
 const getSpaceById = require('../controllers/spaces/get-space-by-id-controller');
 const getSpaces = require('../controllers/spaces/get-spaces-controller');
@@ -16,7 +14,7 @@ const router = express.Router();
 router.route(`/`).get(getSpaces);
 
 // Endpoints privados
-router.route(`/:spaceId/reviews`).all(validateAuth).post(createreviewSpaceById);
+router.route(`/:spaceId/reviews`).all(validateAuth).post(createReviewSpaceById);
 
 router.route(`/:id`).all(validateAuth).get(getSpaceById).delete(deleteSpaceByID);
 router.route(`/:id/reviews`).all(validateAuth).get(getReviewsBySpaceId);
